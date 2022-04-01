1c0e9a78ca41e367d807b1f6d6cb5b032d362c7d
Manjot Singh <manjot.singh@technossus.com>
2022-04-01 13:53:47 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,76 @@
+import { HTTP_INTERCEPTORS,HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MsalGuard, MsalInterceptor, MsalModule, MsalRedirectComponent } from '@azure/msal-angular';
+import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { AzureAdDemoService } from './azure-ad-demo.service';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+const isIE=window.navigator.userAgent.indexOf('MSIE')>-1
+||window.navigator.userAgent.indexOf('Trident/')>-1
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatDividerModule,
+    MsalModule.forRoot(new PublicClientApplication
+      (
+        {
+          auth:{
+            clientId:'',
+            redirectUri:'http://localhost:9000',
+            authority:''
+          },
+          cache:
+          {
+            cacheLocation:'localStorage',
+            storeAuthStateInCookie:isIE
+          }
+        }
+      ),
+      {
+        interactionType:InteractionType.Redirect,
+        authRequest:{
+          scopes:['user.read']
+        }
+      },
+      {
+        interactionType:InteractionType.Redirect,
+        protectedResourceMap:new Map(
+          [
+            ['https://graph.microsoft.com/v1.0/me',['user.Read']],
+          ]
+        )
+      }
+      )
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{
+    provide:HTTP_INTERCEPTORS,
+    useClass:MsalInterceptor,
+    multi:true
+  },MsalGuard,AzureAdDemoService],
+  bootstrap: [AppComponent,MsalRedirectComponent]
 })
 export class AppModule { }
+
